64de5553686f51730756009650fec5c4844b1b44
Markkerss <marcolaju2003@gmail.com>
2021-03-18 16:12:51 +0700

    client setup complete

---
+++ jankenpon-client/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Swal from 'sweetalert2'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ jankenpon-client/src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from '../axios/axios'
+import router from '@/router/index.js'
+import Swal from 'sweetalert2'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
