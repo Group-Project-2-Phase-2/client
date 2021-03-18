aa84d85d5d60c48f05eef680516d42f80d965dc2
hubbusysyuhada <hubbusysyuhada@gmail.com>
2021-03-18 22:16:42 +0700

    fixing bug and adding async proccess to find and entering room

--- jankenpon-client/src/store/index.js
+++ jankenpon-client/src/store/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import axios from '../axios/axios'
+import axios from '../axios/axios'
+import router from '../router'
 // import router from '@/router/index.js'
 // import Swal from 'sweetalert2'
+const baseURL = 'http://localhost:3000/'
 
 Vue.use(Vuex)
 
@@ -12,7 +14,25 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
-
+    fetchEnemy (context, payload) {
+      console.log(localStorage.access_token, "<<<<<<<<<<<<<<")
+      axios({
+        method : 'post',
+        url : baseURL + 'matchmake',
+        headers : {
+          access_token : localStorage.access_token
+        }
+      })
+      .then(({data}) => {
+        console.log(data, payload);
+        router.push('match')
+      })
+      .catch(err => {
+        console.log("error");
+        console.log(err);
+        router.push('match')
+      })
+    }
   },
   modules: {
   }
--- jankenpon-client/src/views/Loading.vue
+++ jankenpon-client/src/views/Loading.vue
@@ -11,7 +11,7 @@
 <script>
 export default {
     created () {
-        
+        this.$store.dispatch('fetchEnemy')
     }
 }
 </script>
